cffcb5e2b2213e25e56dd3a96f34ab939be3e2aa
peacelove05 <irishmaesigongan@gmail.com>
2024-11-12 11:56:53 +0800

    added constructor for calendar

---
+++ web.js
@@ -0,0 +1,9 @@
+
+const activityForm = document.getElementById('activity-form');
+const activityInput = document.getElementById('activity');
+const monthYearLabel = document.getElementById('month-year');
+const prevMonthBtn = document.getElementById('prev-month');
+const nextMonthBtn = document.getElementById('next-month');
+const calendarGrid = document.querySelector('.calendar-grid');
+const calendarDays = document.querySelector('.calendar-days');
+
